c4d564bec308d4148f297a775931acd4317fa2f7
sviat <s.d.kubar@gmail.com>
2026-03-27 20:04:52 +0200

    news

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,13 +1,45 @@
 import "./news.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function News (){
-    return (
-        <div className="infoDiv">
-            <p className="infoP">Interacting with our pets</p>
-            <img src="" className="infoImg" />
-            <p className="info">Rescue pups pose as ghosts in festive photo shoot</p>
-            <button className="infobth">see more</button>
+axios.defaults.baseURL = "https://newsapi.org/v2";
+
+const NewsList = ({ item }) => (
+
+    <div className="infoDiv">
+        <p className="infoP">Interacting with our pets</p>
+        <div className="newsDiv">
+        {item.map(({ description, urlToImage, id }) => (
+            <div className="cardNews" key={id}>
+                <img src={urlToImage} className="infoImg" />
+                <p className="info">{description}</p>
+            </div>  
+        ))}
         </div>
-    )
+        <button className="infobth">see more</button>
+
+    </div>
+
+)
+
+function News() {
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+        const Data = async () => {
+            const response = await axios.get("/everything?q=ukraine&pageSize=4&apiKey=3d623b20477b4a1793bce4cf6d726729");
+            console.log(response)
+            setNews(response.data.articles);
+
+        }
+
+        Data();
+    }, []);
+    return (
+        <div className="newsContainer">
+            <NewsList item={news} />
+        </div>)
+
+
 }
 export default News
