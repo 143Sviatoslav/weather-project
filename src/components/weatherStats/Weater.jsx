a8af2f54546a43341629a30145a9eccbd45b56da
sergey123423213 <grinya.0895@gmail.com>
2026-04-04 14:39:09 +0300

    Comment out Weather component in Weater.jsx
    Commented out the Weather component code in Weater.jsx.

--- src/components/weatherStats/Weater.jsx
+++ src/components/weatherStats/Weater.jsx
@@ -1,41 +1,40 @@
-import "./weather.css";
+// import "./weather.css";
 
-
-function Weather() {
-    return (
-        <div className="statusContener">
-            <div className="statusDivQ">
-                <p className="fell">Feels like</p>
-                <p className="degreesC"></p>
-                <img src="" className="cImg" />
-            </div>
-            <div className="statusDivW">
-                <p className="min">Min ℃</p>
-                <p className="degreesMin">27.9℃</p> 
-                <p className="max">Min ℃</p>
-                <p className="degreesMax">27.9℃</p>
-            </div>
-            <div className="statusDivE">
-                <p className="humidity">Humidity</p>
-                <p className="procents">59%</p>
-                <img src="" className="wImg" />
-            </div>
-            <div className="statusDivR">
-                <p className="prssure">Pressure</p>
-                <p className="pa">1007 Pa</p>
-                <img src="" className="paImg" />
-            </div>
-            <div className="statusDivT">
-                <p className="windSpeed">Wind speed</p>
-                <p className="mS">3.17 m/s</p>
-                <img src="" className="windImg" />
-            </div>
-            <div className="statusDivY">
-                <p className="visibility">Visibility</p>
-                <p className="vision">Unlimited</p>
-                <img src="" className="eyeImg" />
-            </div>
-        </div>
-    )
-}
-export default Weather
+// function Weather() {
+//     return (
+//         <div className="statusContener">
+//             <div className="statusDivQ">
+//                 <p className="fell">Feels like</p>
+//                 <p className="degreesC"></p>
+//                 <img src="" className="cImg" />
+//             </div>
+//             <div className="statusDivW">
+//                 <p className="min">Min ℃</p>
+//                 <p className="degreesMin">27.9℃</p> 
+//                 <p className="max">Min ℃</p>
+//                 <p className="degreesMax">27.9℃</p>
+//             </div>
+//             <div className="statusDivE">
+//                 <p className="humidity">Humidity</p>
+//                 <p className="procents">59%</p>
+//                 <img src="" className="wImg" />
+//             </div>
+//             <div className="statusDivR">
+//                 <p className="prssure">Pressure</p>
+//                 <p className="pa">1007 Pa</p>
+//                 <img src="" className="paImg" />
+//             </div>
+//             <div className="statusDivT">
+//                 <p className="windSpeed">Wind speed</p>
+//                 <p className="mS">3.17 m/s</p>
+//                 <img src="" className="windImg" />
+//             </div>
+//             <div className="statusDivY">
+//                 <p className="visibility">Visibility</p>
+//                 <p className="vision">Unlimited</p>
+//                 <img src="" className="eyeImg" />
+//             </div>
+//         </div>
+//     )
+// }
+// export default Weather
